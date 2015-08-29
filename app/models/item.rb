0018ba8c36ae8e0cba95a1e85ea28ae79c61ea34
Antonio <manentea@gmail.com>
2015-08-29 11:26:24 -0400

    small refactor pt2

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,4 +3,10 @@ class Item < ActiveRecord::Base
   has_many :ingredients, dependent: :destroy
 
   validates :name, presence: true
+
+  def create_ingredients(ingredients)
+    ingredients[:ingredients].split(',').each do |ingredient|
+      Ingredient.create(name: ingredient, item_id: self.id)
+    end
+  end
 end
